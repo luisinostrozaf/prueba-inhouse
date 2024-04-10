d39c7bead5075c61a8a52669e16d0c55911f78ab
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-04-10 14:02:38 -0400

    some changes

--- index.js
+++ index.js
@@ -1,9 +1,73 @@
 import express from 'express';
+import fs from 'fs';
+import bodyparser from 'body-parser';
+
 
 const app = express();
+app.use(bodyparser.json());
+
+const readData = () => {
+    try {
+        const data = fs.readFileSync('./db.json');
+        return JSON.parse(data);
+    } catch (error) { //gestor de erorr en caso de que no se encuentre el archivo json
+        console.log('Error reading file', error);
+    }
+}
+
+const writeData = (data) => {
+    try {
+        fs.writeFileSync('./db.json', JSON.stringify(data));
+    } catch (error) {
+        console.log('Error writing file', error);
+    }
+}
 
 app.get('/', (req, res) => {
     res.send('Gestion de usuarios');
+});
+
+//Leer todos los usuarios registrados
+app.get('/users', (req, res) => {
+    const data = readData();
+    res.json(data.users);
+});
+
+//Crear un nuevo usuario
+app.post('/users', (req, res) => {
+    const data = readData();
+    const user = req.body;
+    const newUser = {
+        id: data.users.length + 1,
+        ...user,
+    };
+    data.users.push(newUser);
+    writeData(data);
+    res.json(newUser);
+});
+
+//Actualizar un usario existente
+app.put('/users/:id', (req, res) => {
+    const data = readData();
+    const User = req.body;
+    const id = req.params.id;
+    const Userindex = data.users.findIndex((user) => user.id == id);
+    data.users[Userindex] = {
+        ...data.users[Userindex],
+        ...User,
+    };
+    writeData(data);
+    res.json(data.users[Userindex]);
+});
+
+//Eliminar un usuario existente
+app.delete('/users/:id', (req, res) => {
+    const data = readData();
+    const id = req.params.id;
+    const Userindex = data.users.findIndex((user) => user.id == id);
+    data.users.splice(Userindex, 1);
+    writeData(data);
+    res.json(data.users);
 });
 
 app.listen(3000, () => {
